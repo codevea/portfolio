76b6b83172846e61f5dc87a93dad2bfd6d2fc694
Nathalie Vrecq <nathalie.vrecq@gmail.com>
2024-12-16 01:18:27 +0100

    Modification CSS

--- partials/_header.php
+++ partials/_header.php
@@ -30,7 +30,7 @@
     <div class="textCenter">
       <img src="pictures/image.webp" class="imageHeader">
       <h1>Nathalie Vrecq | <a href="https://github.com/codevea/portfolio-nathalie-vrecq" target="_blank"><i class="fa-brands fa-github"></i> Dépôt GitHub Portfolio</a>
-      </h1>
+    </h1>
     </div>
   </header>
   <nav class="nav">
