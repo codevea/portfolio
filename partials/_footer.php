69730708f1f70bb2bb21a08422bee867e86f67d9
Nathalie Vrecq <nathalie.vrecq@gmail.com>
2024-12-19 15:33:11 +0100

    Correction texte et CSS

--- partials/_footer.php
+++ partials/_footer.php
@@ -2,10 +2,10 @@
   <div class="containerFooter">
     <div>
       Copyright ©2024 Vrecq Nathalie<br>
-      Version 1.0.12<br><br>
+      Version 1.0.13<br><br>
     </div>
     <div>
-      <a href="https://validator.w3.org/nu/?showsource=yes&showoutline=yes&showimagereport=yes&doc=https%3A%2F%2Fwww.portfolio-nathalie-vrecq.codevea.com%2F" target="_blank">W3C Validator</a><br>
+    <i class="fa-solid fa-link"></i>  &nbsp; <a href="https://validator.w3.org/nu/?showsource=yes&showoutline=yes&showimagereport=yes&doc=https%3A%2F%2Fwww.portfolio-nathalie-vrecq.codevea.com%2F" target="_blank">W3C Validator</a><br>
     </div>
   </div>
 </footer>
